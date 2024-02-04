348a5de71cd1ac3a0507ca784512bc5376a7137f
sri-maddineni <121371216+sri-maddineni@users.noreply.github.com>
2024-02-04 12:54:55 +0530

    server and client done

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+import cors from "cors";
+
+//configure environment
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+//routes all
+app.use("/api/v1/auth",authRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>world</h1>");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT} `.bgCyan.white);
+});
